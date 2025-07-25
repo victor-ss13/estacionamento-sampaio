12a04baec626c9c406ca2f534f27fb9e4e03ac08
Victor Sampaio <victor.sampaio2003@gmail.com>
2025-07-25 13:21:43 +0000

    feat: add "VerificaEsc()" in "ConsoleHelper.cs

--- projetoEstacionamento/Utils/ConsoleHelper.cs
+++ projetoEstacionamento/Utils/ConsoleHelper.cs
@@ -16,6 +16,23 @@ namespace projetoEstacionamento.Utils
         {
             Console.WriteLine("\nPressione qualquer tecla para continuar...");
             Console.ReadKey();
-        }   
+        }
+
+        // Função para permitir voltar ao menu principal pressionando ESC
+        // Testar depois
+        public static bool VerificaEsc(string mensagem = "\nPressione ESC para sair ou qualquer outra tecla para continuar...")
+        {
+            Console.WriteLine(mensagem);
+
+            var tecla = Console.ReadKey(intercept: true).Key;
+
+            if (tecla == ConsoleKey.Escape)
+            {
+                Console.Clear();
+                return true;
+            }
+
+            return false;
+        }
     }
 }
